8ce7a58c71a2584ba43a6a0bf4cc4c75e383ca96
etnrx27 <94886504+etnrx27@users.noreply.github.com>
2022-05-06 00:12:05 +0800

    Added PRG2-ASG files for Upload

---
+++ PRG2_T08_Team6/PRG2_T08_Team6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PRG2_T08_Team6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
